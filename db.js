7263aadc421e09366da76892bf92f8a679ba0205
Astha Rai <72337639+astha114@users.noreply.github.com>
2023-08-14 18:00:07 +0530

    Update db.js

--- db.js
+++ db.js
@@ -13,6 +13,7 @@ const connectToMongoDb = async () => {
                         else{
                                 global.food_items = data;
                                 global.foodCategory = catData;
+                                console.log(data,catData)
                         }
 
                 })
