6c0d99fbd15c1e40aa0476e0da56837448e8ef6c
astha <72337639+astha114@users.noreply.github.com>
2023-08-22 01:43:36 +0530

    Revert "v2"
    This reverts commit b11a8dbeaf83cdf8daee911f6aba4c42060297cf.

--- app.js
+++ app.js
@@ -4,20 +4,25 @@ const app = express();
 const PORT = process.env.PORT ;
 const mongoDB = require('./db')
 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+})
 
 mongoDB();
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.redirect('/login')
+app.get('/login',(req,res)=>{
     res.send('hello')
 })
 
 app.use('/api',require('./Routes/CreateUser'));
 app.use('/api',require('./Routes/DisplyData'));
-app.use('/api',require('./Routes/orderdata'));
-
-
+app.use('/api',require('./Routes/OrderData'));
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, "./client/build")));
